d726b54d9a4a575f6a96ffe2f930b7bbbf60801a
amybenzene25 <mahamatahmatamina8@gmail.com>
2022-05-18 08:38:42 -0700

    Mon premier projet sur git

---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+var selectedRow = null;
+
+function onFormSubmit(){
+    console.log(formData);
+    if(validate() && validate1() && validate2()){
+        var formData = readFormData();
+        console.log(formData);
+        if(selectedRow == null){
+            insertNewRecord(formData);
+        }else{
+            updateRecord(formData);
+        }
+        
+        resetForm();
+    }
+}
+
+function readFormData(){
+
+    var formData = {};
+    formData["fullname"] = document.getElementById("fullname").value;
+    formData["code"] = document.getElementById("code").value;
+    formData["gender"] = document.getElementById("gender").value;
+    formData["address"] = document.getElementById("address").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["school"] = document.getElementById("school").value;
+
+    return formData;
+}
+
+function insertNewRecord(data){
+    var table = document.getElementById("emplist").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+
+    cell2 = newRow.insertCell(0);
+    cell2.innerHTML = data.code;
+
+    cell1 = newRow.insertCell(1);
+    cell1.innerHTML = data.fullname;
+
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.gender;
+
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.address;
+
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = data.email;
+
+    cell4 = newRow.insertCell(5);
+    cell4.innerHTML = data.school;
+
+    cell4 = newRow.insertCell(6);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                        <a  onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm(){
+    document.getElementById('code').value = '';
+    document.getElementById('fullname').value = '';
+    document.getElementById('gender').value = '';
+    document.getElementById('address').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('school').value = '';
+    selectedRow = null;
+}
+
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('code').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('fullname').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('gender').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('address').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('email').value = selectedRow.cells[4].innerHTML;
+    document.getElementById('school').value = selectedRow.cells[5].innerHTML;
+}
+
+function updateRecord(formData){
+
+    selectedRow.cells[0].innerHTML = formData.code;
+    selectedRow.cells[1].innerHTML = formData.fullname;
+    selectedRow.cells[2].innerHTML = formData.gender;
+    selectedRow.cells[3].innerHTML = formData.address;
+    selectedRow.cells[4].innerHTML = formData.email;
+    selectedRow.cells[5].innerHTML = formData.school;
+
+}
+
+function onDelete(td){
+    if(confirm('Are you sure to delete this record?')){
+        row = td.parentElement.parentElement;
+        document.getElementById("emplist").deleteRow(row.rowIndex);
+        resetForm();
+    }
+    
+}
+
+function validate(){
+    isValid = true;
+    if(document.getElementById('fullname').value == ""){
+        isValid = false;
+        document.getElementById('fullNameValidationError').classList.remove("hide");
+    }else{
+        isValid = true;
+        if(!document.getElementById('fullNameValidationError').classList.remove("hide")){
+            document.getElementById('fullNameValidationError').classList.add("hide");
+        }
+    }
+
+    return isValid;
+}
+function validate1(){
+    isValid = true;
+    if(document.getElementById('code').value == ""){
+        isValid = false;
+        document.getElementById('codeValidationError').classList.remove("hide");
+    }else{
+        isValid = true;
+        if(!document.getElementById('codeValidationError').classList.remove("hide")){
+            document.getElementById('codeValidationError').classList.add("hide");
+        }
+    }
+
+    return isValid;
+}
+function validate2(){
+    isValid = true;
+    if(document.getElementById('email').value == ""){
+        isValid = false;
+        document.getElementById('emailValidationError').classList.remove("hide");
+    }else{
+        isValid = true;
+        if(!document.getElementById('emailValidationError').classList.remove("hide")){
+            document.getElementById('emailValidationError').classList.add("hide");
+        }
+    }
+
+    return isValid;
+}
